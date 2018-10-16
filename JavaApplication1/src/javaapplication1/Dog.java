4dd6aa1d15504021902a9f4713912ce029107119
Joao Estrela Leite <4estrj82@solent.ac.uk>
2018-10-16 18:07:05 +0100

    Commit 2

--- JavaApplication1/src/javaapplication1/Dog.java
+++ JavaApplication1/src/javaapplication1/Dog.java
@@ -16,38 +16,48 @@ private int weight;
 private int energy;
 private int hunger;
 private boolean isGood;
+private Heart heart;
 // Constructor WITH parameters
 public Dog(String nameIn, int weightIn)
 {
-name = nameIn;
-weight = weightIn;
-energy = 100;
-hunger = 50;
-isGood = true;
+    name = nameIn;
+    weight = weightIn;
+    energy = 100;
+    hunger = 50;
+    isGood = true;
+    heart = new Heart(60);
 }
 // Methods
 public void chasePostman()
 {
-System.out.println("BARK! BARK! BARK! GRRRRRRRRRR!");
-energy--;
-hunger--;
-weight--;
-isGood = false;
+    System.out.println("BARK! BARK! BARK! GRRRRRRRRRR!");
+    energy--;
+    hunger--;
+    weight--;
+    isGood = false;
 }
 public void walk()
 {
-weight--;
-energy--;
+    if( heart.increaseBPM(5) )
+    {
+    weight--;
+    energy--;
+    }
+    else
+    {
+    System.out.println("The dog is under too much strain");
+    }
 }
 public void sleep()
 {
-energy = 100;
-hunger--;
+    energy = 100;
+    hunger--;
+    heart.rest();
 }
 public void eat()
 {
-hunger = 100;
-weight++;
+    hunger = 100;
+    weight++;
 }
 // 'Getter' methods
 public String getName()
